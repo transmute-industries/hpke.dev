dcbd661289f888e05d3c04e80d6cc01e3dc95f6e
Orie Steele <orie@transmute.industries>
2024-02-09 17:42:01 -0600

    :rainbow::rocket:

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // distDir: 'dist',
-  // output: 'export',
+  distDir: 'dist',
+  output: 'export',
 };
 
 export default nextConfig;
